f6933762b0d93af50bcc68559c317f6fb7013008
Tamar Putkaradze <99253555+TTamar@users.noreply.github.com>
2022-10-11 17:34:09 +0400

    add changes for SEO

--- assets/app.js
+++ assets/app.js
@@ -401,8 +401,8 @@ const maxSlider = document.getElementById('max');
 const outputMin = document.getElementById('min-value');
 const outputMax = document.getElementById('max-value');
 
-outputMin.innerHTML = minSlider.value;
-outputMax.innerHTML = maxSlider.value;
+outputMin?.innerHTML = minSlider.value;
+outputMax?.innerHTML = maxSlider.value;
 
 minSlider.oninput = function(){
  outputMin.innerHTML=this.value;    
